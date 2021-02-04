1cfca5e099353f60bb2f99f09763d50211d7629d
Ben Chou <ben7152000@gmail.com>
2021-02-04 17:02:25 +0800

    check

--- models/Record.js
+++ models/Record.js
@@ -1,34 +1,4 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
-// const recordSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   date: {
-//     type: String,
-//     required: true
-//   },
-//   category: {
-//     type: String,
-//     required: true
-//   },
-//   categoryIcon: {
-//     type: String,
-//     required: true
-//   },
-//   amount: {
-//     type: Number,
-//     required: true
-//   }
-// })
-
-// module.exports = mongoose.model('Record', recordSchema)
-
-// Require mongoose
 const mongoose = require('mongoose')
-
-// Define Schema
 const Schema = mongoose.Schema
 const recordSchema = new Schema({
   name: {
@@ -53,5 +23,4 @@ const recordSchema = new Schema({
   }
 })
 
-// Create and export a model
 module.exports = mongoose.model('Record', recordSchema)
