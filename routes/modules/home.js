e4119f8f2cba64b013eaae3ebaf23b4290854615
Ben Chou <ben7152000@gmail.com>
2021-02-04 14:45:20 +0800

    test

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../../models/record.js')
+// const Record = require('../../models/record')
 
 // home
 router.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .then(record => {
-      let totalAmount = 0
-      record.forEach(list => {
-        totalAmount += list.amount
-      })
-      res.render('index', { totalAmount, record })
-    })
+  res.send('ok')
+  // Record.find()
+  //   .lean()
+  //   .then(record => {
+  //     let totalAmount = 0
+  //     record.forEach(list => {
+  //       totalAmount += list.amount
+  //     })
+  //     res.render('index', { totalAmount, record })
+  //   })
+  //   .catch(error => console.error(error))
 })
 
 module.exports = router
