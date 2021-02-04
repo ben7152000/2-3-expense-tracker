74f0d98903ce6dde46ba92739336c8edd9aa635f
Ben Chou <ben7152000@gmail.com>
2021-02-04 15:00:33 +0800

    test

--- models/Record.js
+++ models/Record.js
@@ -1,4 +1,34 @@
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
+// const recordSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: String,
+//     required: true
+//   },
+//   category: {
+//     type: String,
+//     required: true
+//   },
+//   categoryIcon: {
+//     type: String,
+//     required: true
+//   },
+//   amount: {
+//     type: Number,
+//     required: true
+//   }
+// })
+
+// module.exports = mongoose.model('Record', recordSchema)
+
+// Require mongoose
 const mongoose = require('mongoose')
+
+// Define Schema
 const Schema = mongoose.Schema
 const recordSchema = new Schema({
   name: {
@@ -23,4 +53,5 @@ const recordSchema = new Schema({
   }
 })
 
+// Create and export a model
 module.exports = mongoose.model('Record', recordSchema)
