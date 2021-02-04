51acf932e4ae65723db7e7f26d2c216c80ba4c0f
Ben Chou <ben7152000@gmail.com>
2021-02-04 22:44:41 +0800

    test

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,14 +1,18 @@
 const db = require('../../config/mongoose')
 const Category = require('../category')
-const categoryList = require('../category.json')
+const categories = require('../category.json').results
 
 db.once('open', () => {
-  const categories = []
-  categoryList.results.forEach(category => {
-    categories.push(category)
-  })
-  Category.create(categories)
-    .then(() => {
-      return db.close()
-    })
+  categories.forEach(category => Category.create(category))
 })
+
+// db.once('open', () => {
+//   const categories = []
+//   categoryList.results.forEach(category => {
+//     categories.push(category)
+//   })
+//   Category.create(categories)
+//     .then(() => {
+//       return db.close()
+//     })
+// })
