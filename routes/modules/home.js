98ae12b4eface6faa4e7373a73e5019863eb6ec4
Ben Chou <ben7152000@gmail.com>
2021-02-05 00:53:19 +0800

    fix

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
-// const Record = require('../../models/record')
+const Record = require('../../models/record')
 
 // home
 router.get('/', (req, res) => {
-  res.render('index')
-  // Record.find()
-  //   .lean()
-  //   .then(record => {
-  //     let totalAmount = 0
-  //     record.forEach(list => {
-  //       totalAmount += list.amount
-  //     })
-  //     res.render('index', { totalAmount, record })
-  //   })
-  //   .catch(error => console.error(error))
+  Record
+    .find()
+    .lean()
+    .then(record => {
+      let totalAmount = 0
+      record.forEach(list => {
+        totalAmount += list.amount
+      })
+      res.render('index', { totalAmount, record })
+    })
+    .catch(error => console.error(error))
 })
 
 module.exports = router
