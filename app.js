08300e42c812aa3f41805720620834063ee754b4
Ben Chou <ben7152000@gmail.com>
2021-02-04 14:31:14 +0800

    check

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const routers = require('./routes/index.js')
+const routers = require('./routes')
 
 // Mongoose
 require('./config/mongoose')
