487dfe66f1615d32f73252ff59bb8015e92f0d60
Ben Chou <ben7152000@gmail.com>
2021-02-04 18:12:33 +0800

    fix app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const routers = require('./routes/index')
+const Router = require('./routes/index')
 
 // Mongoose
 require('./config/mongoose')
@@ -11,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Body-Parser
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Express-Handlebars
@@ -19,7 +20,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: { same: (a, b)
 app.use(express.static('public'))
 
 // Route
-app.use(routers)
+app.use(Router)
 
 // Listen
-app.listen(PORT, console.log('The server is running on localhost:3000'))
+app.listen(PORT, console.log(`The server is running on localhost:${PORT}`))
