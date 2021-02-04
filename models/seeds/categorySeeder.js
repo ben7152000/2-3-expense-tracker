613583abd40a29cd529083f37172e2379a433db5
Ben Chou <ben7152000@gmail.com>
2021-02-05 00:49:20 +0800

    fix seeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -2,15 +2,9 @@ const db = require('../../config/mongoose')
 const Category = require('../category')
 const categories = require('../category.json').results
 
-const p = new Promise((resolve, reject) => {
-  db.once('open', () => {
-    console.log('mongodb connected !!')
-    resolve()
-  })
+db.once('open', () => {
+  console.log('mongodb connected !!')
+  Category.create(categories)
+    .then(() => db.close())
+  console.log('category is done')
 })
-p.then(() => {
-  categories.forEach(category => new Category(category))
-})
-  .then(() => console.log('category is done'))
-  .then(() => db.close())
-  .catch(err => console.log(err))
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -2,12 +2,9 @@ const db = require('../../config/mongoose')
 const Record = require('../record')
 const records = require('../record.json').results
 
-const p = new Promise((resolve, reject) => {
-  db.once('open', () => {
-    console.log('mongodb connected !!')
-    resolve()
-  })
+db.once('open', () => {
+  console.log('mongodb connected !!')
+  Record.create(records)
+    .then(() => db.close())
+  console.log('record is done !!')
 })
-p.then(() => Record.create(records))
-  .then(() => console.log('record is done'))
-  .catch(err => console.log(err))
