82646542a64673ce91cbd7f73a2db2aaf2c292fa
Ben Chou <ben7152000@gmail.com>
2021-02-04 11:48:33 +0800

    finish

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const MOGODB_URI = process.env.MOGODB_URI || 'mongodb://localhost/expense-tracker'
-mongoose.connect(MOGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 
 db.on('error', () => console.log('The database is not working'))
