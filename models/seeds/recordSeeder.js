d411b72fef667075f9a017816129210938a4e5db
Ben Chou <ben7152000@gmail.com>
2021-02-04 18:53:14 +0800

    fix

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,5 +1,5 @@
 const db = require('../../config/mongoose')
 const Record = require('../record')
-const record = require('../record.json')
+const records = require('../record.json')
 
-db.once('open', () => record.results.forEach(record => Record.create(record)))
+db.once('open', () => records.results.forEach(record => Record.create(record)))
