8bce920c0b39e8c7dd3276aeee840335584a06ef
Ben Chou <ben7152000@gmail.com>
2021-02-05 16:36:51 +0800

    finish

--- models/category.js
+++ models/category.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const categorySchema = new Schema({
-  name: String,
-  icon: String
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,17 +1,10 @@
 const db = require('../../config/mongoose')
 const Record = require('../../record')
 const records = require('./record.json').results
-const recordIcon = require('./category.json').results
 
 db.once('open', () => {
-  const recordList = []
-  records.forEach(record => {
-    const icon = recordIcon.find(category => category.name === record.category).icon
-    record.categoryIcon = icon
-    recordList.push(record)
-  })
   console.log('mongodb connected !!')
-  Record.create(recordList)
+  Record.create(records)
     .then(() => db.close())
   console.log('record is done !!')
 })
