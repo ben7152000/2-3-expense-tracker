b44ace7e7e7e123c863069b860bf3cc384d15b7c
Ben Chou <ben7152000@gmail.com>
2021-02-05 11:57:08 +0800

    fix

--- models/category.js
+++ models/category.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const categorySchema = new Schema({
-  name: { type: String, required: true },
-  icon: { type: String, required: true }
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
 })
 
 const Category = mongoose.model('Category', categorySchema)
