7448217f9f98f25261f24d60d6f5a9a48aaabadf
Ben Chou <ben7152000@gmail.com>
2021-02-05 14:37:31 +0800

    fix

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,22 +1,49 @@
+// const express = require('express')
+// const router = express.Router()
+
+// const Record = require('../../models/record')
+
+// const categoryIcon = require('../../models/seeds/categories.json').results
+
+// // home
+// router.get('/', (req, res) => {
+//   Record.find()
+//     .lean()
+//     .then(record => {
+//       let totalAmount = 0
+//       record.forEach(list => {
+//         totalAmount += list.amount
+//       })
+//       res.render('index', { totalAmount, record, categoryIcon })
+//     })
+//     .catch(error => console.error(error))
+// })
+
+// module.exports = router
+
+// Require Express and Express router
 const express = require('express')
 const router = express.Router()
 
+// Require Record model
 const Record = require('../../models/record')
 
-const categoryIcon = require('../../models/seeds/categories.json').results
+// Require category list
+const categoryList = require('../../models/seeds/categories.json').results
 
-// home
+// Set up routes of homepage
 router.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(record => {
       let totalAmount = 0
-      record.forEach(list => {
-        totalAmount += list.amount
+      record.forEach(record => {
+        totalAmount += record.amount
       })
-      res.render('index', { totalAmount, record, categoryIcon })
+      res.render('index', { record, totalAmount, categoryList })
     })
-    .catch(error => console.error(error))
+    .catch(error => console.log(error))
 })
 
+// Export
 module.exports = router
