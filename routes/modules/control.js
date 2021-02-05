c6a13404d8b8de82096c15863ed321b2b7d653fa
Ben Chou <ben7152000@gmail.com>
2021-02-05 14:42:37 +0800

    fix

--- routes/modules/control.js
+++ routes/modules/control.js
@@ -1,6 +1,6 @@
-const Record = require('../../models/record')
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
 
 // New
 router.get('/new', (req, res) => res.render('new'))
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,49 +1,19 @@
-// const express = require('express')
-// const router = express.Router()
-
-// const Record = require('../../models/record')
-
-// const categoryIcon = require('../../models/seeds/categories.json').results
-
-// // home
-// router.get('/', (req, res) => {
-//   Record.find()
-//     .lean()
-//     .then(record => {
-//       let totalAmount = 0
-//       record.forEach(list => {
-//         totalAmount += list.amount
-//       })
-//       res.render('index', { totalAmount, record, categoryIcon })
-//     })
-//     .catch(error => console.error(error))
-// })
-
-// module.exports = router
-
-// Require Express and Express router
 const express = require('express')
 const router = express.Router()
-
-// Require Record model
 const Record = require('../../models/record')
 
-// Require category list
-const categoryList = require('../../models/seeds/categories.json').results
-
-// Set up routes of homepage
+// home
 router.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(record => {
       let totalAmount = 0
-      record.forEach(record => {
-        totalAmount += record.amount
+      record.forEach(list => {
+        totalAmount += list.amount
       })
-      res.render('index', { record, totalAmount, categoryList })
+      res.render('index', { totalAmount, record })
     })
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
-// Export
 module.exports = router
