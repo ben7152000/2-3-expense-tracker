5ede4adea7da5b169ae34e54de5a04e17312d5ed
Ben Chou <ben7152000@gmail.com>
2021-02-05 15:27:06 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const control = require('./modules/control')
+// const control = require('./modules/control')
 
 router.use('/', home)
-router.use('/list', control)
+// router.use('/list', control)
 
 module.exports = router
